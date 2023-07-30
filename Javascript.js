6fce89528b38c7f173b489095557fad7ea59e618
Saitheja <komallasaiteja@gmail.com>
2023-07-30 09:21:46 +0530

    first commit

---
+++ Javascript.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["Saitheja", "Web developer","Front-end developer", " Back-end Developer", "Full Stack Developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop:true
+  });
